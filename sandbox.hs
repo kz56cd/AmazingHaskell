27a41c788cc3d947ea299f6089c5b563513da47f
kz56cd <kz56cd@gmail.com>
2018-05-05 19:25:57 +0900

    Update sandbox

--- sandbox.hs
+++ sandbox.hs
@@ -8,3 +8,14 @@ doubleSmallNumber x = if x > 100
 doubleSmallNumber' x = (if x > 100
                         then x
                         else x * 2) + 1
+
+-- リスト内包表記
+--
+-- | .. 出力を意味する
+-- <- .. リストからの取り出し、xが受け取る ( = xが束縛している)
+-- , .. 条件の追加 (filterに近い。条件に合致したもののみxは受け取る)
+
+-- doubleList = [x * 2 | x <- [1..10], x * 2 >= 12]s
+
+doubleList = [x * 2 | x <- [1..10], x `mod` 7 == 3]
+boomBangs xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
